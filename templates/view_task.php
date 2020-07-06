eed356a5ba9208794626e68fab28acbf0264e8ff
delower <john.keats@ymail.com>
2020-07-06 18:27:37 +0600

    Bootstrap removed, bug fixed

---
+++ templates/view_task.php
@@ -0,0 +1,52 @@
+<br>
+<h2><?php _e("Task #$id", 'wptodo'); ?></h2>
+<br>
+<div class="clock" id="timer"></div>
+<br>
+<h5><?php echo $wptodo_view_item['0']->title; ?></h5>
+<p class="bg-light"><?php echo $wptodo_view_item['0']->desc; ?></p>
+<p>By <strong><?php echo self::wptodo_from((int) $wptodo_view_item['0']->from); ?></strong> on <em><?php echo self::wptodo_date($wptodo_view_item['0']->date); ?></em> Deadline <em><?php echo self::wptodo_date($wptodo_view_item['0']->until); ?></em>, currently assigned to <em><strong><?php echo self::wptodo_from((int) $wptodo_view_item['0']->for); ?></strong></em></p>
+<br>
+<?php
+if ($wptodo_view_item_comments) {
+	echo "<h6>Comments</h6>";
+	$wptodo_counted = count($wptodo_view_item_comments);
+	$c = 0;
+	echo '<p><ol>';
+	while ($c != $wptodo_counted) {
+		echo '<li><p>' . $wptodo_view_item_comments["$c"]->body . '</p>
+									<small>On ' . self::wptodo_date($wptodo_view_item_comments["$c"]->date) . ' by ' . self::wptodo_from((int) $wptodo_view_item_comments["$c"]->from) . '</small></li>';
+		$c++;
+	}
+	echo '</ol></p>';
+}
+\Inc\Pages\Admin::wptodo_cancel();
+?>
+<br>
+<form action="" id="wptodo_addnewcomment" method="post">
+	<input name="wptodo_comment_author" type="hidden" value="<?php echo \Inc\Pages\Admin::get_user_id(); ?>" />
+	<input name="wptodo_comment_task" type="hidden" value="<?php echo $wptodo_view_item['0']->id; ?>" />
+	<!--table starts-->
+	<table>
+		<tr>
+			<td><textarea cols="40" rows="5" name="wptodo_comment_body" id="wptodo_comment_body" placeholder="Add a Comment" required></textarea></td>
+		</tr>
+		<tr>
+			<td>
+				<input name="wptodo_comment_submit" id="wptodo_comment_submit" value="Add" type="submit">
+			</td>
+</form>
+<td>
+	<form action="" method="post">
+		<input name="cancel" value="Cancel" type="submit" />
+	</form>
+</td>
+</tr>
+</table>
+<!--table Ends-->
+<?php
+\Inc\Pages\Admin::wptodo_countdown_timer(self::wptodo_date($wptodo_view_item['0']->until), $wptodo_view_item['0']->status);
+if (isset($_POST['wptodo_comment_submit'])) {
+	\Inc\Base\Model::wptodo_email($wptodo_view_item['0']->from, $wptodo_view_item['0']->for);
+}
+?>
---
+++ wp-todo.php
@@ -0,0 +1,52 @@
+<?php
+/**
+* @package wptodo
+*/
+/*
+Plugin Name: WP To Do
+Plugin URI: https://delower.me/wp-todo/
+Description: A full featured plugin for creating and managing a "to do" list.
+Version:1.1.5
+Author: Delower
+Author URI: https://delower.me
+License: GPLv2 or later
+Text Domain: wptodo
+*/
+/*
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
+Copyright (C) 2018  delower.
+
+*/
+defined('ABSPATH') or die('Hey, What are you doing here? You Silly Man!');
+
+if(file_exists(dirname(__FILE__).'/vendor/autoload.php')){
+	require_once dirname(__FILE__).'/vendor/autoload.php';
+}
+
+//activate plugin
+function activate_wptodo(){
+	\Inc\Base\Activate::activate();
+}
+register_activation_hook( __FILE__, 'activate_wptodo' );
+//deactivate plugin
+function deactivate_wptodo(){
+	\Inc\Base\Deactivate::deactivate();
+}
+register_deactivation_hook( __FILE__, 'deactivate_wptodo' );
+//instantiate classes
+if( class_exists( 'Inc\\Init' ) ){
+	\Inc\Init::register_services();
+}
